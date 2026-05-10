bc3efa84dd8d8b14da3b43bb2a757de690515471
Rifat_Hossain <rifatahmed21095@gmail.com>
2026-05-10 11:58:41 -0400

    Update Lab4_Analog-To-Analog.py

--- Lab4_Analog-To-Analog.py
+++ Lab4_Analog-To-Analog.py
@@ -1,67 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# User Inputs
 fm = int(input("Enter message frequency (Hz): "))
 fc = int(input("Enter carrier frequency (Hz): "))
 
-# Time axis
-t = np.linspace(0, 1, 1000)
+t = np.linspace(0, 1, 2000)
 
-# Message Signal
+# Message and Carrier Signal
 message = np.sin(2 * np.pi * fm * t)
-
-# Carrier Signal
 carrier = np.sin(2 * np.pi * fc * t)
 
-# AM Modulation
+# AM
 am = (1 + message) * carrier
 
-# FM Modulation
-kf = 80
-fm_signal = np.sin(
-    2 * np.pi * fc * t +
-    kf * np.cumsum(message) / 100
-)
+# FM
+kf = 50
+fm_signal = np.sin(2 * np.pi * fc * t + kf * np.cumsum(message) / len(t))
 
-# PM Modulation
-kp = 10
-pm_signal = np.sin(
-    2 * np.pi * fc * t +
-    kp * message
-)
+# PM
+kp = 5
+pm_signal = np.sin(2 * np.pi * fc * t + kp * message)
 
-# Plotting
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(10, 10))
 
-# Message Signal
-plt.subplot(4, 1, 1)
+plt.subplot(5, 1, 1)
 plt.plot(t, message)
 plt.title("Message Signal")
 plt.grid()
 
-# AM Signal
-plt.subplot(4, 1, 2)
+plt.subplot(5, 1, 2)
+plt.plot(t, carrier)
+plt.title("Carrier Signal")
+plt.grid()
+
+plt.subplot(5, 1, 3)
 plt.plot(t, am)
 plt.title("Amplitude Modulation (AM)")
 plt.grid()
 
-# FM Signal
-plt.subplot(4, 1, 3)
+plt.subplot(5, 1, 4)
 plt.plot(t, fm_signal)
 plt.title("Frequency Modulation (FM)")
 plt.grid()
 
-# PM Signal
-plt.subplot(4, 1, 4)
-plt.plot(t, pm_signal)
+plt.subplot(5, 1, 5)
+plt.plot(t, carrier, label="Carrier")
+plt.plot(t, pm_signal, label="PM Signal")
 plt.title("Phase Modulation (PM)")
+plt.legend()
 plt.grid()
 
-# Zoom for clear view
-plt.xlim(0, 0.3)
+plt.xlim(0, 0.2)
 
 plt.tight_layout()
 plt.show()
 
-#Sample input 2 20
+#sample input 2 and 20
